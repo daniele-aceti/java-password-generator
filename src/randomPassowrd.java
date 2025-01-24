87f5bbb6d9794c929748004d0448d7104c6983fc
daniele-aceti <daniele.aceti@hotmail.it>
2025-01-24 11:15:20 +0100

    feat: note

--- src/randomPassowrd.java
+++ src/randomPassowrd.java
@@ -3,9 +3,8 @@ import java.util.Random;
 public class randomPassowrd {
     public static void main(String[] args) {
         Random number = new Random();
-            
             String text = "abcdefghilmnopqrstuvz0123456789-/@!";
-            int max = text.length();
+            int max = text.length();//cosi da ottenere la lunghezza della stringa
             for(int i = 0; i <= 10; i++){
                 int numTest = number.nextInt(max);  
                 char charRandom = text.charAt(numTest);
