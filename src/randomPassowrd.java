978760cd0f8a7ec96231d97dddfd802ee21ae37d
daniele-aceti <daniele.aceti@hotmail.it>
2025-01-24 11:14:20 +0100

    fix: max

--- src/randomPassowrd.java
+++ src/randomPassowrd.java
@@ -3,8 +3,9 @@ import java.util.Random;
 public class randomPassowrd {
     public static void main(String[] args) {
         Random number = new Random();
-            int max = 35;
+            
             String text = "abcdefghilmnopqrstuvz0123456789-/@!";
+            int max = text.length();
             for(int i = 0; i <= 10; i++){
                 int numTest = number.nextInt(max);  
                 char charRandom = text.charAt(numTest);
